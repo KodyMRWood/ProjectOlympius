6c9991894c4733995e8d618ed431c1c8b05b9648
Kody Wood <kodymrwood14@gmail.com>
2023-07-27 12:27:16 -0400

    Added Weapon Collison Boxes and Overlapping to Start Attack Registering

--- Source/ProjectOlympius/Private/Items/Weapons/Weapon.cpp
+++ Source/ProjectOlympius/Private/Items/Weapons/Weapon.cpp
@@ -2,10 +2,26 @@
 
 
 #include "Items/Weapons/Weapon.h"
-#include "Components/SphereComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
 #include "Characters/OlympiusCharacter.h"
 
+AWeapon::AWeapon()
+{
+	WeaponBox=CreateDefaultSubobject<UBoxComponent>(TEXT("Hit Box"));
+	WeaponBox->SetupAttachment(GetRootComponent());
+	WeaponBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	WeaponBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	WeaponBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	BoxTraceStart = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace Start"));
+	BoxTraceStart->SetupAttachment(GetRootComponent());
+	BoxTraceEnd = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace End"));
+	BoxTraceEnd->SetupAttachment(GetRootComponent());
+
+}
 
 void AWeapon::Equip(TObjectPtr<USceneComponent> InParent, FName InSocketName)
 {
@@ -25,6 +41,12 @@ void AWeapon::Equip(TObjectPtr<USceneComponent> InParent, FName InSocketName)
 	}
 }
 
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	WeaponBox->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnBoxOverlap);
+}
+
 void AWeapon::AttachMeshToSocket(TObjectPtr<USceneComponent> InParent, const FName& InSocketName)
 {
 	FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
@@ -39,4 +61,26 @@ void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* 
 void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	Super::OnSphereEndOverlap(OverlappedComponent,OtherActor, OtherComp, OtherBodyIndex);
+}
+
+void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FVector Start = BoxTraceStart->GetComponentLocation();
+	const FVector End = BoxTraceEnd->GetComponentLocation();
+
+	TArray<TObjectPtr<AActor>> ActorsToIgnore;
+	ActorsToIgnore.Add(this);
+	FHitResult BoxHit;
+	UKismetSystemLibrary::BoxTraceSingle(
+		this,
+		Start,
+		End,
+		FVector(5.0f, 5.0f, 5.0f),
+		BoxTraceStart->GetComponentRotation(),
+		ETraceTypeQuery::TraceTypeQuery1,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		BoxHit,
+		true);
 }
--- Source/ProjectOlympius/Public/Items/Weapons/Weapon.h
+++ Source/ProjectOlympius/Public/Items/Weapons/Weapon.h
@@ -1,12 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
+/*
+Author(s): Kody Wood
+Description: Functionalty for the Weapon actor, Child of Item
+*/
 
 #include "CoreMinimal.h"
 #include "Items/Item.h"
 #include "Weapon.generated.h"
 
 class USoundBase;
+class UBoxComponent;
 /**
  * 
  */
@@ -15,12 +18,27 @@ class PROJECTOLYMPIUS_API AWeapon : public AItem
 {
 	GENERATED_BODY()
 public:
+	AWeapon();
 	void Equip(TObjectPtr<USceneComponent> InParent, FName InSocketName);
 	void AttachMeshToSocket(TObjectPtr<USceneComponent> InParent, const FName& InSocketName);
 protected:
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override; 
-	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+	virtual void BeginPlay() override;
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override; 
+	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex) override;
+	
+	UFUNCTION()
+	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 private:
 	UPROPERTY(EditAnywhere, Category ="Weapon Properties")
 	TObjectPtr<USoundBase> EquipSound;
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	TObjectPtr<UBoxComponent> WeaponBox;
+	UPROPERTY(VisibleAnywhere)
+		TObjectPtr<USceneComponent> BoxTraceStart;
+	UPROPERTY(VisibleAnywhere)
+		TObjectPtr<USceneComponent> BoxTraceEnd;
 };
